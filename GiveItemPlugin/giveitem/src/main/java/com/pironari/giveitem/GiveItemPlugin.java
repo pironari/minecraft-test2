31872aeef0f276b1b772d02e63114130f31e916a
piro <ogakura.5656@gmail.com>
2026-03-21 20:00:01 +0900

    GiveItemPlugin 初期セットアップ

---
+++ GiveItemPlugin/giveitem/src/main/java/com/pironari/giveitem/GiveItemPlugin.java
@@ -0,0 +1,16 @@
+package com.pironari.giveitem;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class GiveItemPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        getLogger().info("GiveItemPlugin が有効になりました！");
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("GiveItemPlugin が無効になりました！");
+    }
+}
